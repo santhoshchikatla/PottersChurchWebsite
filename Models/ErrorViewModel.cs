230899e918122b3be23a8127fc72fdf40b082257
Santhosh Kumar <santhoshchikatla1@gmail.com>
2026-03-18 00:24:37 +0530

    Removed .vs and added gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PottersChurch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
